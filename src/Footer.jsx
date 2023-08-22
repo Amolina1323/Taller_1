4f022d1a40cc1b804e7c98640f37e14ff2383044
Amolina1323 <129243377+Amolina1323@users.noreply.github.com>
2023-08-22 17:08:35 -0500

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = ( ) => {
+    return(
+        <main>
+        <footer>
+        <section class="foo">
+            <br></br>
+            <p class="copyright">
+                <strong>Developed by:Andres Felipe Molina Melo @2023 Apprentice Sena</strong>
+            </p>
+        </section>
+        </footer>
+        </main>
+    )
+}
+export default Footer;
